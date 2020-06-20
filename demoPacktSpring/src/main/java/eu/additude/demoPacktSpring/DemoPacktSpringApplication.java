de40ca35bbe3fc93c734bc34401286dbaa451521
Rutger S <rutger.spaans@additude.eu>
2020-06-20 15:10:59 +0200

    Springboot Maven

---
+++ demoPacktSpring/src/main/java/eu/additude/demoPacktSpring/DemoPacktSpringApplication.java
@@ -0,0 +1,13 @@
+package eu.additude.demoPacktSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPacktSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPacktSpringApplication.class, args);
+	}
+
+}
---
+++ demoPacktSpring/src/test/java/eu/additude/demoPacktSpring/DemoPacktSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.additude.demoPacktSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPacktSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
