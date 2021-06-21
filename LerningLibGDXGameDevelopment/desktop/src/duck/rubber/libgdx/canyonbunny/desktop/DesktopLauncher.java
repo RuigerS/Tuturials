d0517b5aa0feb00cb256063a7dd6d3a6556e6efb
Rutger S <rutger.spaans@hotmail.com>
2021-06-21 09:21:28 +0200

    Adding new book Learning LibGDX Development - 2nd ed

---
+++ LerningLibGDXGameDevelopment/desktop/src/duck/rubber/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package duck.rubber.libgdx.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import duck.rubber.libgdx.canyonbunny.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=800;
+		config.height=480;
+		config.title="Canyon Bunny";
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
