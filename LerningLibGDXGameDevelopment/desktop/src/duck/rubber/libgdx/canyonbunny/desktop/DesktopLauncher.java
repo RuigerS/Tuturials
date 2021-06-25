3883dde6638e2fff28b1efc394583ad86da0d7f3
Rutger S <rutger.spaans@hotmail.com>
2021-06-25 09:33:01 +0200

    LibGDX GameDevelopment 2ndE with menu's

--- LerningLibGDXGameDevelopment/desktop/src/duck/rubber/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ LerningLibGDXGameDevelopment/desktop/src/duck/rubber/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package duck.rubber.libgdx.canyonbunny.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import duck.rubber.libgdx.canyonbunny.CanyonBunnyMain;
@@ -10,6 +11,9 @@ public class DesktopLauncher {
 		config.width=800;
 		config.height=480;
 		config.title="Canyon Bunny";
+		config.addIcon("icons/ic_launcher_128.png", Files.FileType.Internal);
+		config.addIcon("icons/ic_launcher_32.png", Files.FileType.Internal);
+		config.addIcon("icons/ic_launcher_16.png", Files.FileType.Internal);
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
